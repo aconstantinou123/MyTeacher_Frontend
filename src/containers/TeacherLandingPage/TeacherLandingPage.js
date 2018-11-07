3dddfe616acc06f16075a092dfa84eeb1ff8ad39
Alex Constantinou <alexconstantinou@Alexs-MBP.lan>
2018-11-07 21:36:13 +0000

    landing page started

--- src/containers/TeacherLandingPage/TeacherLandingPage.js
+++ src/containers/TeacherLandingPage/TeacherLandingPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import * as videoChatActionCreators from '../../actions/videoChatActions'
 import * as teacherActionCreators from '../../actions/teacherActions'
 import * as authenticationActionCreators from '../../actions/authenticationActions'
@@ -9,60 +10,34 @@ import * as authenticationActionCreators from '../../actions/authenticationActio
 import './TeacherLandingPage.scss'
 
 class TeacherLandingPage extends Component {
-  constructor() {
-    super()
-    this.handleDisconnect = this.handleDisconnect.bind(this)
-  }
 
   componentWillMount() {
-    const { generateToken, teacherLogin } = this.props
-    generateToken()
-    const body = {
-      username: 'zonio-strings',
-      password: 'password',
+    const { generateToken, history, teacher } = this.props
+    if(!teacher){
+      history.push('/login')
     }
-    teacherLogin(body)
-  }
-
-  handleDisconnect() {
-    const { activeRoom, disconnectFromRoom } = this.props
-    activeRoom.disconnect()
-    disconnectFromRoom(activeRoom)
+    generateToken()
   }
 
   render() {
-    const { connectToRoom, hasJoinedRoom } = this.props
+    const { teacher } = this.props
     return (
       <div className="test">
-Video Chat Test
-        <div ref={(remoteMedia) => { this.remoteMedia = remoteMedia }} className="media-container" />
-        <div ref={(localMedia) => { this.localMedia = localMedia }} className="media-container" />
-        {
-          !hasJoinedRoom
-          && <button type="button" onClick={() => connectToRoom(this.localMedia, this.remoteMedia)}>Connect</button>
-        }
-        {
-          hasJoinedRoom
-          && <button type="button" onClick={this.handleDisconnect}>Disconnect</button>
-        }
+      <h1>Welcome {teacher.firstName}</h1>
+        <div>
+          <h2>My Students</h2>
+          <Link to="class">Start Class</Link>
+        </div>
       </div>
     )
   }
 }
 
-TeacherLandingPage.defaultProps = {
-  activeRoom: null,
-}
 
 TeacherLandingPage.propTypes = {
-  activeRoom: PropTypes.object,
-  disconnectFromRoom: PropTypes.func.isRequired,
   generateToken: PropTypes.func.isRequired,
-  connectToRoom: PropTypes.func.isRequired,
-  hasJoinedRoom: PropTypes.bool.isRequired,
-  // getTeacher: PropTypes.func.isRequired,
-  teacherLogin: PropTypes.func.isRequired,
-
+  teacher: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
 }
 
 function mapStateToProps(state) {
--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -6,12 +6,14 @@ import PropTypes from 'prop-types'
 import TeacherLandingPage from '../containers/TeacherLandingPage/TeacherLandingPage'
 import TeacherLogin from '../containers/TeacherLogin/TeacherLogin'
 import WelcomePage from '../containers/WelcomePage/WelcomePage'
+import VirtualClassroom from '../containers/VirtualClassroom/VirtualClassroom';
 
 const MainRoutes = ({ history }) => (
   <div>
     <Route exact path="/" component={WelcomePage} />
     <Route history={history} path="/login" component={TeacherLogin} />
     <Route history={history} path="/teacher" component={TeacherLandingPage} />
+    <Route history={history} path="/class" component={VirtualClassroom} />
   </div>
 )
 

29bfafe84a4008725b6a753c155c87825c1a8fc3
Alex Constantinou <alexconstantinou@Alexs-MBP.lan>
2018-11-26 00:33:32 +0000

    linter passed

--- src/actions/scheduleActions.js
+++ src/actions/scheduleActions.js
@@ -1,26 +1,20 @@
 import moment from 'moment'
 
 import {
-  SET_INITIAL_DATE, 
+  SET_INITIAL_DATE,
   GO_BACK_ONE_WEEK,
   GO_FORWARD_ONE_WEEK,
 } from '../types/types'
 
-export const setInitialDate = () => {
-  return {
-    type: SET_INITIAL_DATE,
-    payload: moment().startOf('isoWeek')
-  }
-}
+export const setInitialDate = () => ({
+  type: SET_INITIAL_DATE,
+  payload: moment().startOf('isoWeek'),
+})
 
-export const advanceCalenderOneWeek = () => {
-  return {
-    type: GO_FORWARD_ONE_WEEK,
-  }
-}
+export const advanceCalenderOneWeek = () => ({
+  type: GO_FORWARD_ONE_WEEK,
+})
 
-export const goBackOneCalenderWeek = () => {
-  return {
-    type: GO_BACK_ONE_WEEK,
-  }
-}
+export const goBackOneCalenderWeek = () => ({
+  type: GO_BACK_ONE_WEEK,
+})
--- src/containers/Schedule/Schedule.js
+++ src/containers/Schedule/Schedule.js
@@ -6,45 +6,46 @@ import { bindActionCreators } from 'redux'
 import * as scheduleActionCreators from '../../actions/scheduleActions'
 
 class Schedule extends Component {
-  constructor(){
+  constructor() {
     super()
     this.renderWeek = this.renderWeek.bind(this)
     this.advanceOneWeek = this.advanceOneWeek.bind(this)
     this.goBackOneWeek = this.goBackOneWeek.bind(this)
   }
 
-  componentWillMount(){
+  componentWillMount() {
     const { setInitialDate } = this.props
     setInitialDate()
   }
 
-  advanceOneWeek(){
+  advanceOneWeek() {
     const { advanceCalenderOneWeek } = this.props
     advanceCalenderOneWeek()
   }
 
-  goBackOneWeek(){
+  goBackOneWeek() {
     const { goBackOneCalenderWeek } = this.props
     goBackOneCalenderWeek()
   }
 
-  renderWeek(){
+  renderWeek() {
     const { initialDate } = this.props
     const daysOfTheWeek = _.range(0, 7, 1)
     return daysOfTheWeek.map((day) => {
       const newDay = initialDate.clone()
-      return <th key={day}>{newDay.add(day, 'day').format('dddd Do MMM').toString()}</th> 
+      return <th key={day}>{newDay.add(day, 'day').format('dddd Do MMM').toString()}</th>
     })
   }
 
 
-  render(){
+  render() {
     const { initialDate } = this.props
     return (
-        <div>
-          <h2>My Schedule</h2>
-          {
-            initialDate &&
+      <div>
+        <h2>My Schedule</h2>
+        {
+            initialDate
+            && (
             <div>
               <table>
                 <tbody>
@@ -53,37 +54,38 @@ class Schedule extends Component {
                   </tr>
                 </tbody>
               </table>
-            <button type='button' onClick={this.goBackOneWeek}>Previous Week</button>
-            <button type='button' onClick={this.advanceOneWeek}>Next Week</button>
+              <button type="button" onClick={this.goBackOneWeek}>Previous Week</button>
+              <button type="button" onClick={this.advanceOneWeek}>Next Week</button>
             </div>
+            )
           }
-        </div>
+      </div>
 
-      )
-    }
+    )
   }
+}
 
-  Schedule.defaultProps = {
-    initialDate: null
-  }
+Schedule.defaultProps = {
+  initialDate: null,
+}
 
-  Schedule.propTypes = {
-    setInitialDate: PropTypes.func.isRequired,
-    initialDate: PropTypes.object,
-    advanceCalenderOneWeek: PropTypes.func.isRequired,
-    goBackOneCalenderWeek: PropTypes.func.isRequired,
-  }
+Schedule.propTypes = {
+  setInitialDate: PropTypes.func.isRequired,
+  initialDate: PropTypes.object,
+  advanceCalenderOneWeek: PropTypes.func.isRequired,
+  goBackOneCalenderWeek: PropTypes.func.isRequired,
+}
 
-  function mapStateToProps(state) {
-    return {
-      ...state.schedule,
-    }
+function mapStateToProps(state) {
+  return {
+    ...state.schedule,
   }
-  
-  function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-      ...scheduleActionCreators,
-    }, dispatch)
-  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    ...scheduleActionCreators,
+  }, dispatch)
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Schedule)
